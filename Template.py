b26ac2d38d2142e4952213b2203344af4e608d48
Octavio <33660690+Occy88@users.noreply.github.com>
2018-01-24 19:05:15 +0000

    first example...
    nothing special...
    run on XAMPP remember to convert to .cgi extension

---
+++ Template.py
@@ -0,0 +1,26 @@
+#!C:\Users\octav\AppData\Local\Programs\Python\Python36-32\python.exe -u
+#!/usr/bin/env python
+# Required header that tells the browser how to render the text.
+import cgi
+import cgitb
+cgitb.enable()
+
+def htmlTop():
+    print("""Content-type:text/html\n\n
+    <!DOCTYPE html>
+    <html>
+        <head lang="en">
+            <meta charset="utf-8"/>
+            <title>My first server page</title>
+        </head>
+        <body>""")
+def htmlTail():
+    print("""</body>
+        </html>""")
+#main program
+if __name__=="__main__":
+    try:
+        htmlTop()
+        htmlTail()
+    except:
+        cgi.print_exception()
